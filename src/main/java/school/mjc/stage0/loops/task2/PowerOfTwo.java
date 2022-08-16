78d2d77eeaf82b49793a3c25e4dec41d35b9e3f7
Yoqub <yyboboraximov@gmail.com>
2022-08-16 18:16:55 +0500

    task2.3

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,17 +1,19 @@
 package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
+
+    public static void main(String[] args) {
+        PowerOfTwo powerOfTwo=new PowerOfTwo();
+        powerOfTwo.printPower(8);
+    }
+
     public void printPower(int power) {
-
-        if(power==31)
-            System.out.println("Yes");
-        else
-            System.out.println("No");
-
-        if(power==64)
-            System.out.println("Yes");
-        else
-            System.out.println("No");
-
+        int i=1;
+        for (int a = 0; a < power; a++) {
+            i *= 2;
+        }
+        if (i<0)
+            System.out.println("too much power");
+        System.out.println(i);
     }
 }
