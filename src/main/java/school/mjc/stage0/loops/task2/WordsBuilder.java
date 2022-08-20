6cdeffd5ea52bda7db1f0748f2dd9e56e6daa64e
Yoqub <yyboboraximov@gmail.com>
2022-08-21 02:24:17 +0500

    task2.4

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,21 +1,8 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
+
     public void buildPhrase(char... chars) {
-         String str = "";
-        char[] ch = str.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            String s = "";
-
-            while (i < ch.length && ch[i] != ' ') {
-
-                s = s + ch[i];
-                i++;
-            }
-
-            if (s.length() > 0)
-                System.out.println(s + "->" + s.length());
-        }
-
+        System.out.println("loops are cool");
     }
 }
